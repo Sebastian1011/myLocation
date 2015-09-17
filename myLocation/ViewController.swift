ab739b54a776d386a231b4a5af1c015e057a2003
ziming zhang <zzmbpt@163.com>
2015-09-17 17:31:02 +0800

    create new project

---
+++ myLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myLocation
+//
+//  Created by 张子名 on 9/17/15.
+//  Copyright (c) 2015 tingdao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
